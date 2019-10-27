673713721aedefb7ecc0f636c097ef540da76de5
Anisimova Viktoria <vika5b@list.ru>
2019-10-27 23:22:08 +0500

    calc

---
+++ CalcTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace CalcTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculator.Calc calculator = new Calculator.Calc();
+            Assert.Equal(5, calculator.Sum(2, 3));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Calculator.Calc calculator = new Calculator.Calc();
+            Assert.Equal(-10, calculator.Sum(-20, 10));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
